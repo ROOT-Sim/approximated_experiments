b61a5849c9e45325fbdbc009e3cca44a1e2fc1a5
Romolo Marotta <romolo.marotta@gmail.com>
2024-02-22 14:37:44 +0100

    Added pcs model
    * added src of pcs
    * modified scripts to launch pcs
    
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- run_experiments.py
+++ run_experiments.py
@@ -9,6 +9,8 @@ phold_approximated_percentages = [0.25, 0.5, 0.75, 1.0]
 tbc_lps = 16384
 tbc_modes = ["PRECISE", "AUTONOMIC", "MANUAL-A", "MANUAL-B"]
 
+pcs_lps = [64, 256, 4096]
+
 experiments_config = {
     "base": {
         "threads": [],
@@ -114,24 +116,44 @@ def rootsir_run(param_str, model_folder, collect_tbc=False):
         os.system("rm -f tbc_stats.txt")
     os.system(f"mv root_sir_stats_phases.txt data/{model_str}_phases.txt")
     os.system(f"mv root_sir_stats.bin data/{model_str}.bin")
+    os.system(f"python3 rootsim_core/src/log/parse/rootsim_stats.py data/{model_str}.bin")
     os.system(f"mv *.txt data/")
 
 
 # Collect data for all configurations and model versions
 def collect_data(test):
-    lps=[10]
+    lps=[320, 640] #[10, 20, 40, 80, 160, 320, 640]
+    if "pcs" in test:
+        lps = pcs_lps
     os.system("mkdir -p data")
     for lp in lps:
         for iteration in range(experiments_config[test]["repetitions"]):
             for num_threads in experiments_config[test]["threads"]:
                 for m in ["BOUNDED"]:
-                    for r in ["RANGE_1_1pC", "RANGE_0_1", "RANGE_1_1pI", "RANGE_0_1pI"]:
+                    #for r in ["RANGE_1_1pC", "RANGE_0_1", "RANGE_1_1pI", "RANGE_0_1pI"]:
+                    for r in ["RANGE_0_1pI"]:
                         write_config_base(lp, num_threads, r, m)
                         param_str = f"{m}_{r}_{lp}_{num_threads}_{iteration}"
                         rootsir_run(param_str, test)
-                        model_str = f"data/{test}_{param_str}_evo.txt"
-                        os.system(f"python3 process-abs.py {model_str}")
-                        os.system(f"python3 process-rel.py {model_str}")
+                        #os.system(f"python3 process-abs.py {model_str}")
+                        if "pcs" not in test:
+                            model_str = f"data/{test}_{param_str}_evo.txt"
+                            os.system(f"python3 process-rel.py {model_str}")
+    print(f"{test} experiments completed")
+
+# Collect data for all configurations and model versions
+def collect_data_pcs():
+    lps = pcs_lps
+    test="pcs"
+    r = "RANGE_0_1pI" 
+    os.system("mkdir -p data")
+    for lp in lps:
+        for iteration in range(experiments_config[test]["repetitions"]):
+            for num_threads in experiments_config[test]["threads"]:
+                for m in range(2):
+                    write_config_base(lp, num_threads, r, m)
+                    param_str = f"{m}_{r}_{lp}_{num_threads}_{iteration}"
+                    rootsir_run(param_str, test)
     print(f"{test} experiments completed")
 
 def collect_phold_data():
@@ -182,7 +204,8 @@ def collect_tbc_long_data():
 
 load_configuration()
 prepare_rootsim()
-collect_data("base_v02")
+#collect_data("base_v02")
+collect_data_pcs()
 #collect_data("incr")
 #collect_phold_data()
 #collect_tbc_data()
